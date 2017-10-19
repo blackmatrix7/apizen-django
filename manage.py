2ccd9f5571b15f0dcb039dc40635b6928647925a
Matrix <Matrix@BlackMatrix.local>
2017-10-19 09:56:56 +0800

    初始化项目

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2017/10/19 上午9:42
+# @Author : Matrix
+# @Github : https://github.com/blackmatrix7/
+# @Blog : http://www.cnblogs.com/blackmatrix/
+# @File : manage.py
+# @Software: PyCharm
+
+__author__ = 'blackmatrix'
+
+if __name__ == '__main__':
+    pass
