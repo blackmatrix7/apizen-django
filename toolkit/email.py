7ff75aaf4f4b4e937184df04b37380d6cb4e45df
Matrix <codecolor@outlook.com>
2018-03-21 17:03:19 +0800

    修改发送邮件函数

--- toolkit/email.py
+++ toolkit/email.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 from django.conf import settings
 from django.template import loader
-from django.core.mail import send_mass_mail
+from django.core.mail import EmailMultiAlternatives
 
 __author__ = 'blackmatrix'
 
@@ -17,5 +17,8 @@ __all__ = ['send_template_mail']
 
 def send_template_mail(subject, template_name, recipient_list, **kwargs):
     from_email = settings.EMAIL_HOST_USER
-    message = loader.render_to_string(template_name=template_name, context=kwargs)
-    send_mass_mail(datatuple=((subject, message, from_email, recipient_list),))
+    html_content = loader.render_to_string(template_name=template_name, context=kwargs)
+    msg = EmailMultiAlternatives(subject, html_content, from_email, recipient_list)
+    msg.content_subtype = "html"
+    msg.send()
+
