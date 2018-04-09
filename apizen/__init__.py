76b80676d5a16e8af74bc982d8dfdb85d825bdbc
Matrix <codecolor@outlook.com>
2018-04-09 14:00:10 +0800

    更新导入模块的方法

--- apizen/__init__.py
+++ apizen/__init__.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from importlib import import_module
 
 
-def auto_import_methods():
-    for methods in settings.APIZEN_METHODS:
-        import_module(methods)
+def auto_register_methods():
+    for method in settings.APIZEN_METHODS:
+        import_module(method)
