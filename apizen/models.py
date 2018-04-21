4d3000fb4e7df4b77e8cf90dbdefc1c15540efa0
Matrix <codecolor@outlook.com>
2018-04-21 10:50:37 +0800

    增加相关的表

--- apizen/models.py
+++ apizen/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-# class Request(models):
-#
-#     request_id = models.CharField(max_length=32, verbose_name='request_id')
+class Request(models):
+    request_id = models.CharField('request id', max_length=32)
+    method = models.CharField('method', max_length=4)
+    api_name = models.CharField('api name', max_length=128)
+    query_string = models.CharField('query string', max_length=512, null=True, blank=True)
+    arguments = models.TextField('arguments', null=True, blank=True)
+    access_time = models.DateTimeField('access time', auto_now_add=True)
+    status = models.IntegerField('http status')
+
